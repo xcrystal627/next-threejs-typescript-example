09c1720fd07a3afb9466904f8f97b62e6fddeb94
xcrystal <xcrystal627@gmail.com>
2024-04-23 22:50:08 -0700

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import ThreeScene from '@/components/ThreeScene'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <main>
+      <ThreeScene/>
+    </main>
+  )
+}
